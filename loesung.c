3c2da271bd77111b6ac2673a9cdc72ebda2ab6f7
Justin <myusername@DESKTOP-DC2KNUB.localdomain>
2021-06-04 00:41:27 +0200

    Tried some things out

--- loesung.c
+++ loesung.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+int scanContents() {
+    char id[40];
+    char nodeIDs[40][20];
+
+    if (scanf("%[^:]", id) != 1) {
+        fprintf(stderr, "Error");
+    }
+    while (getchar() != '\n') {
+        if (scanf("%[^,\n]", id) != 1) {
+            fprintf(stderr, "Error");
+        }
+    }
+
+    printf("%s ", id);
+    for (int i = 0; i < 20; i++) {
+        printf("%s", nodeIDs[i]);
+    }
+
+    return 0;
+}
+
 int main() {
 
 }
