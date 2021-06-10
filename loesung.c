933368bc598aa074c59c9ae8efa31ed419cc0565
Justin <myusername@DESKTOP-DC2KNUB.localdomain>
2021-06-11 01:29:35 +0200

    Scans stdin and prints the IDs and neighbourIDs for now

--- loesung.c
+++ loesung.c
@@ -1,26 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct Node;
+
+struct Node {
+    char *id;
+    uint32_t value;
+    struct Node *neighbours;
+    uint32_t numNeighbours;
+};
 
 int scanContents() {
-    char id[40];
-    char nodeIDs[40][20];
+    struct Node node;
+    node.id = malloc(sizeof(*node.id) * 100);
+    char* rest = malloc(sizeof(*rest) * 100);
 
-    if (scanf("%[^:]", id) != 1) {
-        fprintf(stderr, "Error");
-    }
-    while (getchar() != '\n') {
-        if (scanf("%[^,\n]", id) != 1) {
-            fprintf(stderr, "Error");
+    while (scanf("%[0-9a-z]", node.id) == 1) {
+        printf("NodeID: %s\n", node.id);
+
+        if (getchar() != ':') {
+            // TODO: ERROR
         }
-    }
 
-    printf("%s ", id);
-    for (int i = 0; i < 20; i++) {
-        printf("%s", nodeIDs[i]);
+        int finished = 0;
+        while(!finished) {
+            scanf("%[0-9a-z]", rest);
+            printf("Rest: %s\n", rest);
+
+            char x = getchar();
+            if (x == '\n') {
+                finished = 1;
+            } else if (x == '-') {
+                int value;
+
+                if(scanf("%d", &value) == 1) {
+                    // TODO: ERROR
+                }
+
+                node.value = value;
+                printf("NodeValue: %d\n", node.value);
+                if (getchar() != '\n') {
+                    // TODO: ERROR
+                }
+
+                finished = 1;
+            } else if (x != ',') {
+                // TODO: ERROR
+            }
+
+        }
+        printf("\n");
     }
 
     return 0;
 }
 
 int main() {
-
+    scanContents();
 }
