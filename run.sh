be4b12e4fd09050953916e27775b1be8a63c3895
Justin <myusername@DESKTOP-DC2KNUB.localdomain>
2021-07-25 23:47:14 +0200

    argument test runs all testfiles and compares them, argument check checks for memory leaks

--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 if [ "$1" == test ]; then
+  > result.txt # clear result.txt
+
+  # add all results of testfiles to result.txt
   for f in example_testcases/*.stdin;
   do
-    ls "$f"; ./loesung < "$f" ; echo;
+    echo "running $f"; (./loesung < "$f") >> result.txt ;
+  done
+
+  # check for differences
+  echo check:
+  diff result.txt realRes.txt
+  echo check complete.
+
+elif [ "$1" == check ]; then
+  # check all testfiles for memory leaks
+  for f in example_testcases/*.stdin;
+  do
+    echo "running $f"; ( (valgrind ./loesung) > /dev/null) < "$f" ;
   done
 else
-  echo use test for testing through all testfiles
+  echo use \'test\' for testing through all testfiles
 fi
